d69c0de48f4fd6469833f41da0eb8c74dc0dfeb4
jahbotrasec <stopcar74@yahoo.com.br>
2024-05-21 20:42:55 -0300

    v1.0

---
+++ contagem.py
@@ -0,0 +1,7 @@
+# usuário informa um número 
+contador = int(input('informe um numero inteiro: '))
+
+# conta a partir do número informado  até 0
+while contador >= 0:
+    print(contador)
+    contador -= 1
